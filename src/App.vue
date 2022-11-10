2ce304f6333ed66ed3599dac413b4e1d1c6171ad
Bamidele <bamsjovicy@gmail.com>
2022-11-10 01:38:59 +0100

    made some arrangement to the folders

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HeaderBar />
+    <div class="main-section columns">
+      <main class="column">
+        <Heroes />
+      </main>
+    </div>
+  </div>
+</template>
+  
+<script>
+  import HeaderBar from "@/components/header-bar";
+  import Heroes from "@/components/heroes-section";
+
+  export default {
+    name: "App",
+    components: { HeaderBar, Heroes },
+  };
+</script>
+  
+<style lang="scss">
+  @import "@/design/index.scss";
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
+</style>
+  
